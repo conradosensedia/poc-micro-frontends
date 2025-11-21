d192a8d96cb1eb532b127491bdb410ed506f7fa8
Conrado Basso <conrado.basso@sensedia.com>
2025-11-21 15:38:01 -0300

    adicionado react bootstrap

--- host/src/App.js
+++ host/src/App.js
@@ -1,11 +1,42 @@
+import { Navbar, Nav, Container, Row, Col, ListGroup } from "react-bootstrap";
+
 export default function App() {
   return (
-    <div className="container mt-5">
-      <div className="alert alert-primary">
-        Host com Bootstrap funcionando!
-      </div>
+    <>
+      {/* NAVBAR */}
+      <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
+        <Container fluid>          
+          <Navbar.Toggle aria-controls="main-nav" />
+          <Navbar.Collapse id="main-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#">Home</Nav.Link>              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-      <h1 className="text-center">Hello World</h1>
-    </div>
+      {/* Espaço para a navbar fixa */}
+      <div style={{ marginTop: "70px" }}></div>
+
+      {/* LAYOUT */}
+      <Container fluid>
+        <Row>
+          {/* SIDEBAR */}
+          <Col xs={2} className="bg-light border-end vh-100 p-3">            
+            <ListGroup variant="flush">
+              <ListGroup.Item action>APIs</ListGroup.Item>
+              <ListGroup.Item action>Apps</ListGroup.Item>
+              <ListGroup.Item action>Planos</ListGroup.Item>
+            </ListGroup>
+          </Col>
+
+          {/* CONTENT */}
+          <Col xs={10} className="p-4">
+            <h1>Hello World</h1>
+            <p>Conteúdo principal do Host App</p>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
